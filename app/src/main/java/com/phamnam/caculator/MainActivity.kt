6429bee24c29b8d61cf9f0cd3382570595a2cd00
Phambanam <Phambanam1999@gmail.com>
2021-06-30 13:27:55 +0300

    Caculator GUI

---
+++ app/src/main/java/com/phamnam/caculator/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.phamnam.caculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
